44fbde63c7fc306832d1b65ee29c0f64e8dfb438
Tom Gurney <6736496+tom-gurney@users.noreply.github.com>
2025-07-21 14:57:57 +0100

    Update image_tag function to match new ImageTag API
    Update Twig image_tag function signature to accept sizes string,
    widths array, and attributes matching the new responsive image
    generation in giantpeach/images package.

--- src/Functions/Image.php
+++ src/Functions/Image.php
@@ -4,49 +4,63 @@ namespace Giantpeach\Schnapps\Twiglet\Functions;
 
 class Image extends \Twig\Extension\AbstractExtension
 {
-  public function getFilters(): array
-  {
-    return [
-      new \Twig\TwigFilter('image', [$this, 'handleImage']),
-    ];
-  }
-
-  public function getFunctions(): array
-  {
-    return [
-      new \Twig\TwigFunction('image', [$this, 'getImage']),
-    ];
-  }
-
-  public function getImage(array|int|string $image, string $size)
-  {
-    if (class_exists('\Giantpeach\Schnapps\Images\Facades\Images')) {
-      if (is_array($image)) {
-        $image = $image['id'];
-      }
-
-      return \Giantpeach\Schnapps\Images\Facades\Images::getImageUrlForSize($image, $size);
-      //return \Giantpeach\Schnapps\Images\Facades\Images::get(image: $image, imageSize: $size);
+    public function getFilters(): array
+    {
+        return [
+            new \Twig\TwigFilter('image', [$this, 'handleImage']),
+        ];
     }
 
-    return null;
-  }
-
-  public function handleImage($data, $w = 500, $h = 500, $crop = true, $webp = false)
-  {
-    if (class_exists('\Giantpeach\Schnapps\Images\Images')) {
-      $opts = [
-        'w' => $w,
-        'h' => $h,
-        'crop' => $crop,
-      ];
-
-      if ($webp) {
-        $opts['fm'] = 'webp';
-      }
-      return $imgUrl = \Giantpeach\Schnapps\Images\Images::getInstance()->getGlideImageUrl($data, $opts);
+    public function getFunctions(): array
+    {
+        return [
+            new \Twig\TwigFunction('image', [$this, 'getImage']),
+            new \Twig\TwigFunction('image_tag', [$this, 'imageTag']),
+        ];
     }
 
-    return $data;
-  }
+    public function getImage(array|int|string $image, string $size)
+    {
+        if (class_exists('\Giantpeach\Schnapps\Images\Facades\Images')) {
+            if (is_array($image)) {
+                $image = $image['id'];
+            }
+
+            return \Giantpeach\Schnapps\Images\Facades\Images::getImageUrlForSize($image, $size);
+            //return \Giantpeach\Schnapps\Images\Facades\Images::get(image: $image, imageSize: $size);
+        }
+
+        return null;
+    }
+
+    public function imageTag(array|int|string $image, string $sizes = '100vw', array $widths = [375, 750, 1100, 1500, 2200], array $attributes = []): string
+    {
+        if (class_exists('\Giantpeach\Schnapps\Images\Facades\Images')) {
+            if (is_array($image)) {
+                $image = $image['id'];
+            }
+
+            return \Giantpeach\Schnapps\Images\Facades\Images::createImageTag($image, $sizes, $widths, $attributes);
+        }
+
+        return '';
+    }
+
+    public function handleImage($data, $w = 500, $h = 500, $crop = true, $webp = false)
+    {
+        if (class_exists('\Giantpeach\Schnapps\Images\Images')) {
+            $opts = [
+                'w' => $w,
+                'h' => $h,
+                'crop' => $crop,
+            ];
+
+            if ($webp) {
+                $opts['fm'] = 'webp';
+            }
+            return $imgUrl = \Giantpeach\Schnapps\Images\Images::getInstance()->getGlideImageUrl($data, $opts);
+        }
+
+        return $data;
+    }
 }
